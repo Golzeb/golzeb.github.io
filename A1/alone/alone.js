8ed1d282b62885afcf82d08291790988bbc7677f
Golzeb <golzebhown@gmail.com>
2018-05-08 18:33:50 +0200

    Added index, changed alone

--- A1/alone/alone.js
+++ A1/alone/alone.js
@@ -5,29 +5,3 @@ iframe.src = "https://www.youtube.com/embed/xe4iTkOHfEM?autoplay=1&version=3&loo
 iframe.setAttribute("frameborder", "0")
 iframe.setAttribute("allow", "autoplay")
 document.body.appendChild(iframe)
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function a() {
-    document.getElementById("banner").style.opacity = 0.0;
-    document.getElementById("bg").style.filter = "brightness(80%)";
-    await sleep((Math.random() * (200 - 50))+ 50);
-    document.getElementById("banner").style.opacity = 1.0;
-    document.getElementById("bg").style.filter = "brightness(110%)";
-    await sleep((Math.random() * (200 - 50))+ 50);
-    document.getElementById("banner").style.opacity = 0.25;
-    document.getElementById("bg").style.filter = "brightness(90%)";
-    await sleep((Math.random() * (200 - 50))+ 50);
-    document.getElementById("banner").style.opacity = 1.0;
-    document.getElementById("bg").style.filter = "brightness(110%)";
-}
-
-(function loop() {
-    var rand = Math.round(Math.random() * (12000 - 3000)) + 3000;
-    setTimeout(function() {
-            a();
-            loop();  
-    }, rand);
-}());
