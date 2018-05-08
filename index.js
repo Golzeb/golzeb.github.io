b2b7b8327e879c1c2a273cf681f91b4124da1120
Golzeb <golzebhown@gmail.com>
2018-05-08 18:39:05 +0200

    Added sound to index

---
+++ index.js
@@ -0,0 +1,9 @@
+var iframe = document.createElement('iframe')
+document.body.style.overflow = "hidden"
+iframe.onload = function() { iframe.width = 0; iframe.height = 0; }
+iframe.src = "https://www.youtube.com/embed/_W1P7AvV17w?autoplay=1&version=3&loop=1&playlist=_W1P7AvV17w"
+iframe.setAttribute("frameborder", "0")
+iframe.setAttribute("allow", "autoplay")
+iframe.style.position = "absolute";
+iframe.style.top = "1500px";
+document.body.appendChild(iframe)
