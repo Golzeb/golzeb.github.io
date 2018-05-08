575bba626bbab0d18c808a525f63287028508db2
Golzeb <golzebhown@gmail.com>
2018-05-08 18:50:10 +0200

    Small fix

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ iframe.src = "https://www.youtube.com/embed/_W1P7AvV17w?autoplay=1&version=3&loo
 iframe.setAttribute("frameborder", "0")
 iframe.setAttribute("allow", "autoplay")
 iframe.style.position = "absolute";
-iframe.style.top = "1500px";
+iframe.style.top = "1079px";
 document.body.appendChild(iframe)
