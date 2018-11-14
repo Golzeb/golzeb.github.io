e6a3d4d87e1456535a56dd4df89bf2b09c24c5e2
Golzeb <golzebhown@gmail.com>
2018-11-14 21:15:55 +0100

    Rewrite

--- mappool/script.js
+++ mappool/script.js
@@ -1,10 +1,14 @@
 function generate()
 {
     elements = document.getElementsByClassName("mapset");
-                
+    
+    console.log(elements);
+
     for(i = 0; i < elements.length; i++) {
-        backgroundString = '\'https://assets.ppy.sh/beatmaps/' + elements[i].classList.item(1) + '/covers/cover.jpg\'';
-        elements[i].innerHTML = '<div class="background-image" style="background-image: url(' + backgroundString + ');"></div>' + elements[i].innerHTML;         
+        backgroundString = 'https://assets.ppy.sh/beatmaps/' + elements[i].classList.item(1) + '/covers/cover.jpg';
+        background = 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("' + backgroundString + '");'; 
+        elements[i].setAttribute("style", "background: " + background + "; background-size: cover;")
+
         var a = document.createElement("a");
         var parent = elements[i].parentNode;
 
